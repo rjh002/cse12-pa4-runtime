ad2d405ec2d554e847f0251f183e26a7105c0364
rjh002 <rjh002@ucsd.edu>
2023-05-24 14:49:45 -0700

    Update Measure.java

--- pa4-starter/pa4-student/src/cse12pa4student/Measure.java
+++ pa4-starter/pa4-student/src/cse12pa4student/Measure.java
@@ -10,16 +10,71 @@ public class Measure {
 		/** TODO **/
 		
 		//Example call to mystery method
-		mysteryA(50);
-		return null;
+		List<Measurement> finalList = new ArrayList<Measurement>();
+			for(int i = 0; i< toRun.length; i++) {
+				
+				String s = toRun[i];
+				
+				for(int n = start N; n <= stopN; n++) {
+					
+					long startTime = System.nanoTime();
+					long nanoSecondsToRun = 0;
+					
+					if(s.equals("A"))
+					{
+						mysteryA(n);
+						nanoSecondsToRun = System.nanoTime() - startTime;
+					}
+					else if(s.equals("B"))
+					{
+						mysteryB(n);
+						nanoSecondsToRun = System.nanoTime() - startTime;
+					}
+					else if(s.equals("C"))
+					{
+						mysteryB(n);
+						nanoSecondsToRun = System.nanoTime() - startTime;
+					}
+					else if(s.equals("D"))
+					{
+						mysteryB(n);
+						nanoSecondsToRun = System.nanoTime() - startTime;
+					}
+					else if(s.equals("E"))
+					{
+						mysteryB(n);
+						nanoSecondsToRun = System.nanoTime() - startTime;
+					}
+					else if(s.equals("F"))
+					{
+						mysteryB(n);
+						nanoSecondsToRun = System.nanoTime() - startTime;
+					}
+					
+					Measurement m = new Measurements(s, n, nanoSecondsToRun);
+					
+					finalist.add(m);
+				}
+			}
+			return finalist;
 	}
 	
 
 	public static String measurementsToCSV(List<Measurement> toConvert) {
 		/** TODO **/
-		return null;
+		
+		String returnVal = "name,n,nanoseconds\n";
+		
+		for (int i = 0; i < toConvert.size(); i++)
+		{
+			returnVal += toConvert.get(i).name + "," + toConvert.get(i).valueOfN + "," + toConvert.get(i).nanosecondsToRun +"\n";
+		}
+		
+		return returnVal;
+		
 	}
 	
 	/* Add any helper methods you find useful */
 		
 }
+
