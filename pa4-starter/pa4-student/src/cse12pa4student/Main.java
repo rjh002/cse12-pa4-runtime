a34f6b11778c5d9c85f42c625fd795e195f26584
rjh002 <rjh002@ucsd.edu>
2023-05-24 14:49:38 -0700

    Update Main.java

--- pa4-starter/pa4-student/src/cse12pa4student/Main.java
+++ pa4-starter/pa4-student/src/cse12pa4student/Main.java
@@ -2,6 +2,14 @@ package cse12pa4student;
 public class Main {
 	
 	public static void main(String[] args) {
+
+		List<Measurement> mOne = Measure.measure(new String[] {"A","B","C","D","E","F"},0,20);
+		List,Measurement> mTwo = Measure.measure(new String[] {"A","B","C","D","D","E","F"},1000,1020);
+		List<Measurement> mThree = Measure.measure(new String[] {"A","B","C","D","E","F"},10000,10010);
+		
+		System.out.println(Measure.measurementsToCSV(mOne));
+		System.out.println(Measure.measurementsToCSV(mTwo));
+		System.out.println(Measure.measurementsToCSV(mThree));
 		
 	}
 }
